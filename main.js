0cb4782edd3580dba3c989b7f38641e226711873
Thomas-E-Johnson <thomas.e.johnson1@gmail.com>
2018-03-14 21:05:32 +0000

    Final

--- main.js
+++ main.js
@@ -11,15 +11,47 @@ function makeGreen() {
   p.style.color = '#BADA55';
   p.style.fontSize = '50px';
 }
+
 // Regular
+console.log(`hello`);
 // Interpolated
+console.log(`hello i am a %s string`, ``);
+//console.log(`hello i am ${var}`);
 // Styled
+console.log(`%c i am some great text`, `font-size:50px`);
 // warning!
+console.warn(`oh noo`);
 // Error :|
+console.error(`poop`);
 // Info
+console.info(`big poop`);
 // Testing
+console.assert(1 === 2, `that is wrong`);
 // clearing
+console.clear();
 // Viewing DOM Elements
+//console.log(p);
+//console.dir(p);
 // Grouping together
+dogs.forEach(dog => {
+  console.groupCollapsed(`${dog.name}`);
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`);
+  console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+  console.group(`${dog.name}`);
+});
 // counting
+console.count(`Tom`);
+console.count(`Tom`);
+console.count(`Tom`);
+console.count(`Tom`);
 // timing
+console.time('fetching data');
+fetch('djsfsdjhfdshfj')
+  .then(data => data.json())
+  .then(data => {
+    console.timeEnd('fetching data');
+    console.log(data);
+  });
+
+console.table(dogs);
